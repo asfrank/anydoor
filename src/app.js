dec33905edc8a9e8654b9173ef281ec0f35915c5
lishuyu <lishuyu01@qiyi.com>
2019-10-21 21:13:41 +0800

    6.7

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const chalk = require('chalk');
+const path = require('path');
 const conf = require('./config/defaultConfig');
+const route = require('./helper/route');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Http!');
+    const url = req.url;
+    const filePath = path.join(conf.root, url);
+    route(req, res, filePath);
 });
 
 server.listen(conf.port, conf.hostname, () => {
     const addr = `http://${conf.hostname}:${conf.port}`;
     console.log(`Server started at ${chalk.green(addr)}`);
-})
+});
